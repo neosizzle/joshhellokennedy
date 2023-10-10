9cb6819262739e79acf0020602b6c0eae6e6f471
Jya Xuen Lee <jyalee@u82z05s02.42kl.edu.my>
2023-10-10 20:14:47 +0800

    form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [ input, setInput ] = useState('Hello World')
+
+  function onChangeHandler (e) {
+    setInput(e.target.value)
+  }
+
+  function onClickHandler (e) {
+    e.preventDefault()
+    console.log(input)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,9 +26,14 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
+        <form>
+          <label htmlFor="this">This is for form: </label>
+          <input id="this" value={input} onChange={onChangeHandler}/>
+          <button onClick={onClickHandler}>Click me!</button>
+        </form>
       </header>
     </div>
   );
