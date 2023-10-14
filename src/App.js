3eb3d53fc8f876eb2b317edef4c1067e64ab2469
jun han <junanngd1115@gmail.com>
2023-10-14 21:03:33 +0800

    file seperation

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import "./App.css";
-import { useState } from "react";
 
 //Component Imports
-import NameInput from "./components/NameInput";
-import AgeInput from "./components/AgeInput";
+import Form from "./components/form";
 
 function App() {
-  const [input, setInput] = useState("Hello World");
-  const [name, setName] = useState("");
-  const [bool, setBool] = useState(false);
-
-  function nameHandler(e) {
-    const selectedValue = e.target.value;
-    if (selectedValue === "back") {
-      // Handle the backspace option: remove the last character from the name.
-      const updatedName = name.slice(0, -1);
-      setName((name) => updatedName);
-    } else {
-      setName((name) => name + selectedValue);
-    }
-  }
-
-  function submitHandler(e) {
-    e.preventDefault();
-    // Do Something
-  }
-
-  function tester(e) {
-    e.preventDefault();
-    console.log(name);
-    // Do Something
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <form onSubmit={submitHandler}>
-          <NameInput nameHandler={nameHandler} name={name} />
-          {/* <AgeInput/> */}
-          {/* <AddressInput/> */}
-          {/* <EmailInput/> */}
-          {/* <IntraInput/> */}
-          {/* <PhoneNumInput/> */}
-          <button onClick={tester}>Click Me!</button>
-        </form>
-      </header>
+      <Form/>
     </div>
   );
 }
